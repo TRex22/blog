3baddcf55c5194c2fb4fc820e53443d22b5facf6
trex22 <jason@next45.co>
2018-10-14 18:38:18 +0200

    cleanup script and ignore blog

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 gem install jekyll
-mkdir -p blog/
-ln -s images blog/ # mac and others
+# hack because of my nested domain to blog structure and how Hubpress freaks out
+mkdir -p blog/images/
+# ln -s images blog/ # mac and others
 # mount --bind images/ blog/images/ # Linux Only
+cp -rf images/ blog/images/
 jekyll serve
 rm blog/images
+rm -rf blog/
