6f5bba90567e5f314036ded854988fbf9bf6b77c
trex22 <contact@jasonchalom.com>
2019-03-09 17:20:18 +0200

    commit some assets

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# brew install node
+# brew install wget
+# wget --no-check-certificate https://disq.us/url?url=https%3A%2F%2Fghost.org%2Fzip%2Fghost-latest.zip%3AmKy8dmPYiZS80O65Yfqcijztl5A&cuid=3678804 -O code.zip
+# unzip code.zip
+
+npm install --production
+brew install python
+
+sudo pip install buster
+mkdir static
+# buster generate --domain=http://localhost:2368
+# buster preview
+# open http://localhost:9000
+# buster deploy
